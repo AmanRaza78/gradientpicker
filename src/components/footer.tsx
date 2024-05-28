26fcf0dbe38f6e85012946e06c8afc08aa8b53dc
AmanRaza78 <razakhanaman9@gmail.com>
2024-05-28 11:21:19 +0530

    added simple footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="mt-auto py-3 bg-white">
+      <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
+        <p className="text-gray-800">A simple gradient picker created by - Aman Raza Khan</p>
+      </div>
+    </footer>
+  );
+}
